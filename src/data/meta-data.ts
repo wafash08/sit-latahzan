596d26e0dbc9a8752af650caa19ec49645019cfc
wafa <wafa@voltras.co.id>
2025-05-18 10:11:58 +0700

    chat to wa

---
+++ src/data/meta-data.ts
@@ -0,0 +1,28 @@
+type MetaData = {
+  title: string;
+  description: string;
+};
+
+export const homeMetaData: MetaData = {
+  title: "SIT Latahzan",
+  description:
+    "Selamat datang di SIT Latahzán, sekolah Islam terpadu yang membentuk generasi cerdas, berkarakter, dan berakhlak mulia. Temukan informasi terbaru dan kegiatan kami di sini.",
+};
+
+export const profileMetaData: MetaData = {
+  title: "Profil | SIT Latahzan",
+  description:
+    "Pelajari visi, misi, nilai, dan sejarah SIT Latahzán. Kenali lebih dalam sekolah kami yang berkomitmen membina siswa dalam suasana islami dan berprestasi.",
+};
+
+export const achievementMetaData: MetaData = {
+  title: "Prestasi Siswa | SIT Latahzan",
+  description:
+    "Lihat berbagai pencapaian siswa SIT Latahzán di bidang akademik, seni, dan olahraga. Kami bangga dengan prestasi peserta didik kami yang menginspirasi.",
+};
+
+export const ppdbMetaData: MetaData = {
+  title: "PPDB 2025 | SIT Latahzan",
+  description:
+    "Buka pendaftaran siswa baru tahun ajaran 2025 di SIT Latahzán. Daftarkan putra-putri Anda ke sekolah Islam terpadu yang unggul dalam iman, ilmu, dan akhlak.",
+};
---
+++ src/data/ppdb-pics.ts
@@ -0,0 +1,31 @@
+type PIC = {
+  phone: string;
+  message: string;
+};
+
+export function createWhatsappMessage({
+  phone,
+  message,
+}: {
+  phone: string;
+  message: string;
+}): string {
+  const baseUrl = "https://wa.me";
+  const text = encodeURIComponent(message);
+  return `${baseUrl}/${phone}?text=${text}`;
+}
+
+export const picTK: PIC = {
+  phone: "6285775349543",
+  message: `Assalamu'alaikum, boleh tahu informasi lebih lanjut tentang pendafataran TK As-salam Banyuasih Mauk? Terima kasih`,
+};
+
+export const picSd: PIC = {
+  phone: "6285692248934",
+  message: `Assalamu'alaikum, boleh tahu informasi lebih lanjut tentang pendafataran SDIT LaTahzan Mauk? Terima kasih`,
+};
+
+export const picSmp: PIC = {
+  phone: "6289508864213",
+  message: `Assalamu'alaikum, boleh tahu informasi lebih lanjut tentang pendafataran SMPIT LaTahzan Mauk? Terima kasih`,
+};
