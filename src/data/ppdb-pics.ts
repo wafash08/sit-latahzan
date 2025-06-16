29614f062070d76a7279fab3fa1b1522ab4c11a7
wafa <wafa@voltras.co.id>
2025-06-17 06:23:45 +0700

    add whatsapp button

--- src/data/ppdb-pics.ts
+++ src/data/ppdb-pics.ts
@@ -15,17 +15,17 @@ export function createWhatsappMessage({
   return `${baseUrl}/${phone}?text=${text}`;
 }
 
-export const picTK: PIC = {
+export const picTk: PIC = {
   phone: "6285775349543",
-  message: `Assalamu'alaikum, boleh tahu informasi lebih lanjut tentang pendafataran TK As-salam Banyuasih Mauk? Terima kasih`,
+  message: `Assalamu'alaikum Ustazah, boleh tahu informasi lebih lanjut tentang pendafataran TK As-salam Banyuasih Mauk? \nTerima kasih`,
 };
 
 export const picSd: PIC = {
   phone: "6285692248934",
-  message: `Assalamu'alaikum, boleh tahu informasi lebih lanjut tentang pendafataran SDIT LaTahzan Mauk? Terima kasih`,
+  message: `Assalamu'alaikum Ustazah, boleh tahu informasi lebih lanjut tentang pendafataran SDIT LaTahzan Mauk? \nTerima kasih`,
 };
 
 export const picSmp: PIC = {
   phone: "6289508864213",
-  message: `Assalamu'alaikum, boleh tahu informasi lebih lanjut tentang pendafataran SMPIT LaTahzan Mauk? Terima kasih`,
+  message: `Assalamu'alaikum Ustazah, boleh tahu informasi lebih lanjut tentang pendafataran SMPIT LaTahzan Mauk? \nTerima kasih`,
 };
